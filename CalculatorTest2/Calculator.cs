c0603ac17a5d197f9134037ba5c20a0121dce0c1
OldWarrior3000 <xxxx@xxxx.com>
2017-10-17 05:44:40 -0700

    Initial commit

---
+++ CalculatorTest2/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest2
+{
+    public class Calculator
+    { 
+        public int Add(int summand1, int summand2) => summand1 + summand2;
+        public int Substract(int subtrahend1, int subtrahend2) => subtrahend1 - subtrahend2;
+        public int Divide(int divisor1, int divisor2)
+        {
+            if (divisor2 == 0) throw new DivideByZeroException($"{nameof(divisor2)} is 0");
+
+            return divisor1 / divisor2;
+        }
+
+        public int AddAll(params int[] args)
+        {
+            int ReturnValue = 0;
+            //foreach(int item in  args)
+            //{
+            //    ReturnValue += item;
+            //}
+
+            //ReturnValue = args.Select((x) => x).Sum();
+            ReturnValue = args.Sum();
+            return ReturnValue;
+        }
+    
+        
+    }
+}
---
+++ CalculatorTest2Tests/CalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorTest2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest2.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1,2);
+            Assert.AreEqual(3, result); 
+        }
+
+        [TestMethod()]
+        [Owner("OldWarrior")]
+        [TestCategory("Standard")]
+        [Priority(1)]
+        public void SubstractTest()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.Substract(1, 2);    
+            Assert.AreEqual(3, result);     //FAIL
+
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivisionWithWrongParametersTest()
+        {
+            Calculator calculator = new Calculator();
+            calculator.Divide(1,0);
+        }
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.Divide(9, 3);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddAllTest()
+        {
+            Calculator calculator = new Calculator();
+            var Result = calculator.AddAll(1,2,3);
+            Assert.AreEqual(6, Result);
+        }
+    }
+}
